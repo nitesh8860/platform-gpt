7b6d0ceb430e9b6a3a2e01229410fa764463e62f
Nitesh Chauhan <niteshchauhan792@gmail.com>
2023-09-20 09:12:48 +0400

    added logger

--- app/routers/slack.py
+++ app/routers/slack.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter, status, Depends, HTTPException
+import logging
 import json
 import os
 import openai
@@ -16,6 +17,9 @@ from sqlalchemy import func
 from .. import models, schemas, oauth2
 from ..database import get_db
 
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
 router = APIRouter(
     tags=['Slack']
 )
@@ -29,8 +33,6 @@ sentry_sdk.init(
 slack_client = WebClient(token=settings.slack_token)
 app = FastAPI()
 
-# , response_model=schemas.SlackPayloadOut
-
 
 def send_message_to_chatgpt(message):
     """
@@ -41,75 +43,81 @@ def send_message_to_chatgpt(message):
 
     global conversation_buffer
     openai.api_key = settings.openai_api_key
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=message,
-        functions=[
-            {
-                "name": "create_ssh_user",
-                "description": "create ssh user in server",
-                "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "username": {"type": "string", "description": "username of user"},
-                            "serverIP": {"type": "string", "description": "serverIP of server"},
-                            "sshPubKey": {"type": "string", "description": "ssh public key of user"},
-                        },
-                    "required": ["username", "serverIP", "sshPubKey"],
+    response = ""
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=message,
+            functions=[
+                {
+                    "name": "create_ssh_user",
+                    "description": "create ssh user in server",
+                    "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "username": {"type": "string", "description": "username of user"},
+                                "serverIP": {"type": "string", "description": "serverIP of server"},
+                                "sshPubKey": {"type": "string", "description": "ssh public key of user"},
+                            },
+                        "required": ["username", "serverIP", "sshPubKey"],
+                    },
                 },
-            },
-            {
-                "name": "get_k8s_namespaces",
-                "description": "get all namespaces name in a list format for the current cluster context",
-                "parameters": {"type": "object", "properties": {}, },
-            },
-            {
-                "name": "get_k8s_events",
-                "description": "get the events in a table format for the current cluster context",
-                "parameters": {"type": "object", "properties": {}, },
-            },
-            {
-                "name": "get_k8s_pods",
-                "description": "get the pods in a list format for the current cluster context",
-                "parameters": {"type": "object", "properties": {}, },
-            },
-            {
-                "name": "get_secrets",
-                "description": "get the secrets in a table format",
-                "parameters": {"type": "object", "properties": {}, },
+                {
+                    "name": "get_k8s_namespaces",
+                    "description": "get all namespaces name in a list format for the current cluster context",
+                    "parameters": {"type": "object", "properties": {}, },
+                },
+                {
+                    "name": "get_k8s_events",
+                    "description": "get the events in a table format for the current cluster context",
+                    "parameters": {"type": "object", "properties": {}, },
+                },
+                {
+                    "name": "get_k8s_pods",
+                    "description": "get the pods in a list format for the current cluster context",
+                    "parameters": {"type": "object", "properties": {}, },
+                },
+                {
+                    "name": "get_secrets",
+                    "description": "get the secrets in a table format",
+                    "parameters": {"type": "object", "properties": {}, },
+                }
+            ]
+        )
+    except Exception as exc:
+        logger.error(f"bad openai response: {exc}")
+
+    if response.choices[0].message:
+
+        message = response.choices[0].message
+        if 'function_call' in message:
+            logger.info(
+                f"a function call has been detected for {message['function_call']['name']}")
+            available_functions = {
+                "create_ssh_user": create_ssh_user,
+                "get_k8s_namespaces": get_k8s_namespaces,
+                "get_k8s_events": get_k8s_events,
+                "get_k8s_pods": get_k8s_pods,
+                "get_secrets": get_secrets
             }
-        ]
-    )
-    print(response)
+            function_name = message['function_call']['name']
+            fuction_to_call = available_functions[function_name]
+            function_args = json.loads(message["function_call"]["arguments"])
+            if function_name == "create_ssh_user":
+                function_response = fuction_to_call(
+                    username=function_args.get("username"),
+                    serverIP=function_args.get("serverIP"),
+                    sshPubKey=function_args.get("sshPubKey")
+                )
+            if function_name == "get_k8s_namespaces" or function_name == "get_k8s_events" or function_name == "get_k8s_pods" or function_name == "get_secrets":
+                function_response = fuction_to_call()
 
-    message = response.choices[0].message
-    if 'function_call' in message:
-        print("***********function call detected")
-        available_functions = {
-            "create_ssh_user": create_ssh_user,
-            "get_k8s_namespaces": get_k8s_namespaces,
-            "get_k8s_events": get_k8s_events,
-            "get_k8s_pods": get_k8s_pods,
-            "get_secrets": get_secrets
-        }
-        function_name = message['function_call']['name']
-        fuction_to_call = available_functions[function_name]
-        function_args = json.loads(message["function_call"]["arguments"])
-        if function_name == "create_ssh_user":
-            function_response = fuction_to_call(
-                username=function_args.get("username"),
-                serverIP=function_args.get("serverIP"),
-                sshPubKey=function_args.get("sshPubKey")
-            )
-        if function_name == "get_k8s_namespaces" or function_name == "get_k8s_events" or function_name == "get_k8s_pods" or function_name == "get_secrets":
-            function_response = fuction_to_call()
-
-        conversation_buffer.append(
-            {"role": "function", "name": function_name, "content": function_response})
-        gpt_function_response = send_message_to_chatgpt(conversation_buffer)
-        return gpt_function_response
-    print(conversation_buffer)
+            conversation_buffer.append(
+                {"role": "function", "name": function_name, "content": function_response})
+            gpt_function_response = send_message_to_chatgpt(
+                conversation_buffer)
+            return gpt_function_response
+    logger.debug(f"conversation buffer with openai: {conversation_buffer}")
     return response
 
 
@@ -120,7 +128,7 @@ def send_response_to_slack(response, channel_id, event_ts):
         text=response,
         thread_ts=event_ts
     )
-    print(response)
+    logger.debug(f"slack reponse {response}")
     return response
 
 
@@ -135,7 +143,8 @@ def handle_chat(event):
     global conversation_buffer
     if event.event.type and event.event.type != 'test':
         if "goodbye" in event.event.text:
-            print("***********clearing conversation buffer")
+            logger.info(
+                "clearing conversation buffer because 'goodbye' received")
             conversation_buffer = []
             slack_response = send_response_to_slack(
                 "Have a good day!", event.event.channel, event.event.event_ts)
@@ -144,16 +153,16 @@ def handle_chat(event):
             user_message = event.event.text
             content = "user: " + user_message
             conversation_buffer.append({"role": "user",   "content": content})
-            print("***********sending conversation buffer to chatgpt: \n",
-                  conversation_buffer)
+            logger.info(
+                f"sending conversation buffer to chatgpt: {conversation_buffer}")
             gpt_response = send_message_to_chatgpt(
                 conversation_buffer).choices[0].message
             if gpt_response.content:
                 content = gpt_response.content
                 conversation_buffer.append(
                     {"role": "assistant", "content": content})
-                print(
-                    "***********got a response from chatgpt, sending response to slack: \n", content)
+                logger.info(
+                    f"got a response from chatgpt, sending response to slack: {content}")
                 send_response_to_slack(
                     content, event.event.channel, event.event.event_ts)
 
@@ -161,7 +170,7 @@ def handle_chat(event):
 @router.post("/slack_events", status_code=status.HTTP_200_OK)
 async def receive_slack_event(event: schemas.SlackPayload, db: Session = Depends(get_db)):
 
-    print("***********event received from slack: \n", event)
+    logger.info(f"event received from slack: {event}")
     if event.token != settings.slack_event_token:
         raise HTTPException(status_code=403, detail="Invalid token")
     if event.type == "url_verification":
